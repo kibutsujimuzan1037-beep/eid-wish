218ccf421405016c5632c77351a64aabec02e20e
kibutsujimuzan1037-beep <kibutsujimuzan1037@gmail.com>
2026-03-21 00:37:45 +0600

    Create index.html.r

---
+++ index.html.r
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Eid Mubarak</title>
+
+  <style>
+    body {
+      text-align: center;
+      font-family: Arial, sans-serif;
+      background: linear-gradient(135deg, #fdfbfb, #ebedee);
+      padding: 50px;
+    }
+
+    .card {
+      background: white;
+      padding: 40px;
+      border-radius: 20px;
+      box-shadow: 0 10px 25px rgba(0,0,0,0.1);
+      max-width: 600px;
+      margin: auto;
+    }
+
+    h1 {
+      font-size: 40px;
+      color: #2c3e50;
+    }
+
+    p {
+      font-size: 22px;
+      color: #444;
+      line-height: 1.6;
+    }
+
+    .name {
+      font-size: 24px;
+      margin-top: 10px;
+      font-weight: bold;
+    }
+
+    .footer {
+      margin-top: 20px;
+      font-size: 26px;
+      color: #2c3e50;
+    }
+  </style>
+</head>
+
+<body>
+
+  <div class="card">
+    <h1>🌙✨ Eid Mubarak ✨🌙</h1>
+
+    <div class="name">Dear Rinthi 😊</div>
+
+    <p>
+      Wishing you a very happy Eid filled with joy, laughter, and sweet moments 🍬✨ <br><br>
+      Stay as cute and cheerful as you are, and keep smiling always 💖
+    </p>
+
+    <p>
+      May this Eid bring you happiness, success, and lots of good memories 🤍
+    </p>
+
+    <div class="footer">Eid Mubarak once again! 🌙</div>
+  </div>
+
+</body>
+</html>
